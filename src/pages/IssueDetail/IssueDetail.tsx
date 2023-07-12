ac81f353637057fdc074d9adf02ca66c528b841f
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-07-13 04:29:10 +0900

    [style] issueDetail 페이지 ui css styling

--- src/pages/IssueDetail/IssueDetail.tsx
+++ src/pages/IssueDetail/IssueDetail.tsx
@@ -1,11 +1,68 @@
 /* eslint-disable react/no-children-prop */
-import React, { useEffect } from 'react';
+/** @jsxImportSource @emotion/react */
+
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useLoadingContext } from '../../context/LoadingContext';
 import { githubApi } from '../../api/GithubIssueApi';
 import { useIssueContext } from '../../context/IssueContext';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { css } from '@emotion/react';
+
+const flexAlign = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const imgFlexAlign = css`
+  ${flexAlign}
+`;
+
+const imgSize = css`
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+const issueTitleArea = css`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const issueTextArea = css`
+  ${issueTitleArea}
+  font-size: 16px;
+`;
+
+const issueTitle = css`
+  font-weight: 700;
+  margin-left: 10px;
+  white-space: nowrap;
+`;
+
+const issueText = css`
+  ${issueTitle}
+  font-weight: 400;
+  margin: 10px 0 10px 10px;
+`;
+
+const commentStyle = css`
+  white-space: nowrap;
+`;
+
+const line = css`
+  width: 100%;
+  height: 1px;
+  background-color: #c0c0c0;
+  margin: 10px 0;
+`;
 
 export default function IssueDetail() {
   const { isLoading, setLoading } = useLoadingContext();
@@ -35,20 +92,42 @@ export default function IssueDetail() {
     getIssueDetail();
   }, []);
 
+  const date = issueDetailInfo.createdAt
+    ? issueDetailInfo.createdAt.split('T')[0].split('-')
+    : [];
+
   return (
     <div>
       {isLoading && <div>로딩중</div>}
       {!isLoading && (
         <>
-          <div>{issueDetailInfo.author}</div>
-          <div>{issueDetailInfo.comments}</div>
-          <div>{issueDetailInfo.createdAt}</div>
-          <div>{issueDetailInfo.number}</div>
-          <div>{issueDetailInfo.title}</div>
-          <div>{issueDetailInfo.userId}</div>
-          <div>
-            <img src={issueDetailInfo.profileUrl} alt="profile_img" />
+          <div css={flexAlign}>
+            <div>
+              <div css={imgFlexAlign}>
+                <div css={imgSize}>
+                  <a href={issueDetailInfo.profileLink}>
+                    <img
+                      style={{ width: '100%' }}
+                      src={issueDetailInfo.profileUrl}
+                      alt="profile_img"
+                    />
+                  </a>
+                </div>
+                <div css={issueTitleArea}>
+                  <div>#{issueDetailInfo.number}</div>
+                  <div>{issueDetailInfo.title}</div>
+                </div>
+              </div>
+              <div css={issueTextArea}>
+                <div>작성자: {issueDetailInfo.author}</div>
+                <div css={issueText}>
+                  작성일: {`${date[0]}년 ${date[1]}월 ${date[2]}일`}
+                </div>
+              </div>
+            </div>
+            <div css={commentStyle}>코멘트: {issueDetailInfo.comments}</div>
           </div>
+          <div css={line} />
           <ReactMarkdown
             children={issueDetailInfo.body}
             remarkPlugins={[remarkGfm]}
