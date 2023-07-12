4571cfc6ff8737ea9b394e099da7d307863c71d3
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-07-12 13:21:34 +0900

    [feat] react-router-dom v6로 페이지 라우팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import NotFound from './NotFound';
+import IssueList from './pages/IssueList/issueList';
+import IssueDetail from './pages/IssueDetail/IssueDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <IssueList /> },
+      {
+        path: '/issue/:issueId',
+        element: <IssueDetail />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/IssueList/IssueList.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function IssueList() {
+  return <div>IssueList</div>;
+}
