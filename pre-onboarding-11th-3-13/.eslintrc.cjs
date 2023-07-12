5f75ec8439f19dc7eb5201509a3482a021fcad33
Seung taek Oh <elqkddl@naver.com>
2023-07-12 16:33:34 +0900

    init eslint

--- pre-onboarding-11th-3-13/.eslintrc.cjs
+++ pre-onboarding-11th-3-13/.eslintrc.cjs
@@ -1,20 +1,36 @@
-/* eslint-env node */
-
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // 권장 규칙 모음(패키지)
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  // 개별 규칙 (사용자 정의)
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "no-unused-vars": "warn",
+    "react/no-unknown-property": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/anchor-has-content": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        components: ["Link"], // router 사용 시 a 태그 대신 link
+      },
     ],
   },
-}
+};
