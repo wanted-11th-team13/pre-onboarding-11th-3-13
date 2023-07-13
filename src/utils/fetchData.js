c492014c5352d13e872f7708557bb2a6653cb119
Seung taek Oh <elqkddl@naver.com>
2023-07-13 21:02:05 +0900

    [Feat] getIssue 유틸함수 추가

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import { IssueNumber } from '../components/IssueListItem/IssueListItem.styled';
 
 const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+const repoOwner = 'facebook';
+const repo = 'react';
 
 export const getIssueList = async (perPage, Page) => {
   try {
     const response = await axios.get(
-      `https://api.github.com/repos/facebook/react/issues?per_page=${perPage}&page=${Page}&sort=comments`, // per_page = Query parameters
+      `https://api.github.com/repos/${repoOwner}/${repo}/issues?per_page=${perPage}&page=${Page}&sort=comments`, // per_page = Query parameters
       {
         headers: {
           'X-GitHub-Api-Version': '2022-11-28',
@@ -15,6 +18,24 @@ export const getIssueList = async (perPage, Page) => {
       }
     );
     console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
+
+const getIssue = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/${repoOwner}/${repo}/issues/${IssueNumber}`,
+      {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     throw new Error(String(error));
